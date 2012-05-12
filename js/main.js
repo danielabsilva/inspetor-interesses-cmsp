4e970cc75c49af14dace0bf6a66b36247a208afe
Pedro Markun <pedro@esfera.mobi>
2012-05-12 19:41:10 -0300

    adicionada funcao pra carregar campeoes

--- js/main.js
+++ js/main.js
@@ -14,13 +14,63 @@ function termosComuns() {
                 }
             }
   };
-  $.getJSON(my.base_url + '/_search?source=' + JSON.stringify(q), function(data) {
+  
+  $.ajax({  
+    url: my.base_url + '/_search?source=' + JSON.stringify(q),  
+    dataType: 'json',  
+    //data: data,  
+    async: false,  
+    success: function(data) {  
         my.termos_comuns = [];
         $.each(data.facets.assuntos.terms, function(key, data) {
             my.termos_comuns.push(data.term);
         });
-      });
+    }  
+    });
       
+};
+
+function carregaCampeao() {
+    var tags = my.termos_unicos;
+    var q = {
+        "query" : {
+            "term" : {
+                "assuntos" : "tag"
+                    }
+                },
+            size : 0,
+            from: 0,
+            "facets" : {
+                "autores" : {
+                    "terms" : {
+                        "field" : "autores",
+                        "size" : 3
+                        }
+                    }
+                }
+            };
+    
+    var campeao = false;
+    $.each(tags, function(key, data) {
+        if (campeao != true) {
+            var assunto = data;
+            q.query.term.assuntos = assunto;
+            console.log('Trying ' + assunto);
+            $.ajax({  
+                url: my.base_url + '/_search?source=' + JSON.stringify(q),  
+                dataType: 'json',  
+                //data: data,  
+                async: false,  
+                success: function(data){
+                    if (data.facets.autores.terms[0].term == my.vereador) {
+                        $("#termocampeao").text(assunto);
+                        campeao = true;
+                    }
+                }  
+            });
+        }
+    });
+
 };
 
 function carregaResultados() {
@@ -31,6 +81,7 @@ function carregaResultados() {
                     $("#top3 ul").append("<li class='comum'>" + data.term + "(" + data.count + ")" + "</li>");
                 }
                 else {
+                    my.termos_unicos.push(data.term);
                     $("#top3 ul").append("<li>" + data.term + "(" + data.count + ")" + "</li>");
                 }
                 });
