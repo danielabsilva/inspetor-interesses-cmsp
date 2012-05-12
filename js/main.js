cd9df604bba1648edf4a62720c894ffee8a3020a
Pedro Markun <pedro@esfera.mobi>
2012-05-12 19:52:51 -0300

    adicionado funcao pra deixar translucido os temas comuns

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function termosComuns() {
+function carregaComuns() {
   var q = {
       "query" : {
                 "match_all" : {}
@@ -55,11 +55,9 @@ function carregaCampeao() {
         if (campeao != true) {
             var assunto = data;
             q.query.term.assuntos = assunto;
-            console.log('Trying ' + assunto);
             $.ajax({  
                 url: my.base_url + '/_search?source=' + JSON.stringify(q),  
-                dataType: 'json',  
-                //data: data,  
+                dataType: 'json',
                 async: false,  
                 success: function(data){
                     if (data.facets.autores.terms[0].term == my.vereador) {
@@ -74,7 +72,12 @@ function carregaCampeao() {
 };
 
 function carregaResultados() {
-    $.getJSON(my.url, function(data) {
+    $.ajax({  
+        url: my.base_url + '/_search?source=' + JSON.stringify(my.query),  
+        dataType: 'json',  
+        //data: data,  
+        async: false,  
+        success: function(data) {
             $("#nome h2").text(my.vereador);
             $.each(data.facets.autores.terms, function(key, data) {
                 if (my.termos_comuns.indexOf(data.term) > 0) {
@@ -85,7 +88,11 @@ function carregaResultados() {
                     $("#top3 ul").append("<li>" + data.term + "(" + data.count + ")" + "</li>");
                 }
                 });
+        }
         });
     }
 
 
+function removeComuns() {
+    $(".comum").toggleClass("soft");
+}
